bfe55fb9f8e7efeaf130994c7ac066ac6199fa82
Amos <amos.langat@student.moringaschool.com>
2022-08-12 15:11:45 +0300

    complete assignment

---
+++ index.js
@@ -0,0 +1,27 @@
+ let x = 5;
+ y = 3;
+
+ function add(x,y){
+    return x + y;
+ }
+ function subtract(x,y){
+    return x - y;
+ }
+ function multiply(x,y){
+    return x*y;
+ }
+ function divide(x,y){
+    return x/y;
+ }
+function increment(n){
+    return n+1;
+}
+function decrement(n){
+    return n-1;
+}
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
